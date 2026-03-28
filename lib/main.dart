bb7aca5496388a795cb338ce91b1032a32b89594
olan9 <122879282+olan9@users.noreply.github.com>
2026-03-28 10:07:34 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'utils/theme.dart';
 import 'screens/login_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const Mot9App());
 }
 
